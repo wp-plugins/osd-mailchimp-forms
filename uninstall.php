0116df11eb9ea73431e7374ca386692705f9cc64
osdwebdev <osdwebdev@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-29 20:54:02 +0000

    remove echo and error reporting
    git-svn-id: https://plugins.svn.wordpress.org/osd-mailchimp-forms/trunk@998778 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- uninstall.php
+++ uninstall.php
@@ -1,8 +1,4 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', '1');
-//error_reporting(0);
-
 //if uninstall not called from WordPress exit
 if (!defined('WP_UNINSTALL_PLUGIN')) {
     exit();
@@ -12,9 +8,9 @@ if (!defined('WP_UNINSTALL_PLUGIN')) {
 global $wpdb;
 $prefix = $wpdb->base_prefix;
 
-echo $sql = "DELETE 
-		FROM `{$prefix}options` 
-		WHERE `option_name` LIKE 'osd_mc_form%'";
+$sql = "DELETE 
+        FROM `{$prefix}options` 
+        WHERE `option_name` LIKE 'osd_mc_form%'";
 
-echo $wpdb->query($sql); 
+$wpdb->query($sql); 
 exit;
