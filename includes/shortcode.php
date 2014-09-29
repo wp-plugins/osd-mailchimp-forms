73bc3534c2bed5bd7a3ba40c834eb591997b568b
osdwebdev <osdwebdev@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-29 19:54:28 +0000

    Fix php notice
    git-svn-id: https://plugins.svn.wordpress.org/osd-mailchimp-forms/trunk@998751 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/shortcode.php
+++ includes/shortcode.php
@@ -6,7 +6,11 @@ add_filter('widget_text', 'osd_filter_text_widgets');
 
 function osd_replace_shortcode($atts) {
     $userFields = json_decode(get_option($atts['id']), true);
-    $html = "<div class='osd-mc-form-wrapper' data-form-id='{$atts['id']}' data-submit-text='{$atts['submit_text']}' data-class='{$atts['class']}'><div class='osd-mc-loading-message'>Loading Form...</div></div>";
+    $id = (isset($atts['id'])) ? $atts['id'] : '';
+    $submit_text = (isset($atts['submit_text'])) ? $atts['submit_text'] : '';
+    $class = (isset($atts['class'])) ? $atts['class'] : '';
+
+    $html = "<div class='osd-mc-form-wrapper' data-form-id='{$id}' data-submit-text='{$submit_text}' data-class='{$class}'><div class='osd-mc-loading-message'>Loading Form...</div></div>";
     
     // This is for generating the form without AJAX
     // $html = osd_mc_form_load(array(
