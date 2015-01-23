309f8982866f90753ec79e9a1a7f0f0bfde32db7
osdwebdev <osdwebdev@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-23 21:58:35 +0000

    Fixed bug with quotation marks in form specific messaging
    git-svn-id: https://plugins.svn.wordpress.org/osd-mailchimp-forms/tags/2.0.1@1074418 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/form_settings.php
@@ -0,0 +1,268 @@
+<?php
+// Prevent direct access to file
+defined('ABSPATH') or die("No script kiddies please!");
+
+//SETTINGS PAGE
+$formsPage = new OSDMailChimpForms();
+
+class OSDMailChimpForms {
+    private $options;
+
+    public function __construct() {
+        add_action('admin_menu', array($this, 'add_submenu_item'));
+        add_action('admin_init', array($this, 'page_init'));
+    }
+
+    //add options page to wp
+    public function add_submenu_item() {
+        add_submenu_page(
+            'osd-mailchimp-form-options', 
+            'OSD MailChimp Forms', 
+            'Forms', 
+            'manage_options',
+            'osd-mailchimp-forms', 
+            array($this, 'create_admin_page')
+        ); 
+    }
+
+    //create options page
+    public function create_admin_page() {
+        global $wpdb;
+        $mailChimp = new OSDMailChimp();
+        $prefix = $wpdb->base_prefix;
+
+        // Get saved forms
+        $sql = "SELECT * 
+                FROM ".$prefix."options 
+                WHERE option_name LIKE 'osd_mc_forms_%'";
+        $results = $wpdb->get_results($sql, ARRAY_A);
+        
+        //add styling to the page
+        $this->addStyle();
+        ?>
+        <div class="wrap">
+            <h2>OSD MailChimp Forms</h2>   
+            <?php
+            //display any messages
+            if (isset($_GET['settings-updated']) && $_GET['settings-updated'] == 'true') { ?>
+                <div class="updated settings-error" id="setting-error-settings_updated"> 
+                    <p><strong>Settings saved.</strong></p>
+                </div>
+            <?php } ?>
+            <select id="listSelect">
+                <option value="">Select</option>
+                <?php
+                //preload mc lists into select box
+                $lists = ($mailChimp->getLists());
+                foreach($lists as $list) {
+                    echo "<option value='".$list['id']."'>".$list['name']."</option>";
+                }
+                ?>      
+            </select>
+            <div class="button button-secondary" id="addForm">Add Form</div>
+
+            <form id="osd-mc-forms">
+                <?php
+                //add the section  
+                do_settings_sections('osd-mailchimp-forms');
+                //add the nonces
+                wp_nonce_field('save_osd_mc_forms');
+                
+                //add the saved forms
+                foreach($results as $form) {
+                    $formInfo = json_decode($form['option_value'], true);
+                    $formInfo['formName'] = $form['option_name'];
+                    $formHTML = $mailChimp->adminDisplayForm(array('listID' => $formInfo['id'], 'formInfo' => $formInfo));
+                    echo $formHTML;
+                }
+                submit_button(); 
+                ?>
+                <div class='message'></div>
+            </form>
+        </div>
+        <?php
+
+        //add js to the page
+        $this->addJS();
+    }
+
+    //register / add options 
+    public function page_init() {        
+        add_settings_section(
+            'mailchimp_forms', // ID
+            'My Mailchimp Forms', // Title
+            NULL, // Callback
+            'osd-mailchimp-forms' // Page
+        );   
+    }
+
+    public function addJS() {
+        ?>
+        <script type='text/javascript'>
+            jQuery('#addForm').click(function() {
+                get_form(jQuery('#listSelect').val());
+            });
+
+            jQuery('#submit').click(function(e) {
+                e.preventDefault();
+                save_forms();
+            });
+
+            function get_form(list) {
+                if(list == '') {
+                    return;
+                }
+
+                var data = 'action=osd_mc_load_settings_form&list='+list+'&osd_mc_ajax=true&wp_nonce=<?php echo wp_create_nonce('osd_mc_load_settings_form'); ?>';
+                jQuery.ajax({
+                    type: "POST",
+                    url: "<?php echo site_url(); ?>/wp-admin/admin-ajax.php",
+                    data: data
+                }).done(function(response) {
+                    jQuery('p.submit').before(response);
+                });
+            }
+
+            jQuery('#wpcontent').on('click', '.mc-form-remove', removeForm);
+            function removeForm(ev) {
+                var formWrap = jQuery(this).parents('.mcFormWrapper');
+                var shortCode = jQuery(formWrap).find('input[class=shortCode]').val();
+                jQuery(this).html('Removing...');
+                
+                if(shortCode != 'undefined' && shortCode != '') {
+                    document.querySelector('.message').innerHTML = "";
+
+                    var data = "action=delete_mc_form&remove="+shortCode+"&osd_mc_ajax=true&wp_nonce=<?php echo wp_create_nonce('delete_mc_form'); ?>";
+                    jQuery.ajax({
+                        type: "POST",
+                        url: "<?php echo site_url(); ?>/wp-admin/admin-ajax.php",
+                        data: data
+                    }).done(function(response) {
+                        jQuery(formWrap).slideUp("slow", function() {
+                            jQuery(formWrap).remove();
+                        });
+                    });
+                }
+            }
+
+            function save_forms() {
+                var saveForm = jQuery('#osd-mc-forms');
+                var disabled = saveForm.find(':input:disabled').removeAttr('disabled');
+                var data = saveForm.serialize();
+                data += '&osd_mc_ajax=true&action=osd_admin_save_forms&wp_nonce=<?php echo wp_create_nonce('osd_admin_save_forms'); ?>';
+                jQuery.ajax({
+                    type: "POST",
+                    url: "<?php echo site_url(); ?>/wp-admin/admin-ajax.php",
+                    data: data
+                }).done(function(response) {
+                    window.location += "&settings-updated=true";
+                });
+            }
+        </script>
+        <?php
+    }
+
+    public function addStyle() {
+        ?>
+        <style type="text/css">
+            .mcFormWrapper {
+                border-top: 1px solid #ccc;
+                margin-top: 30px;
+                max-width: 50%;
+                min-width: 650px;
+                padding-top: 20px;
+            }
+            .mcFormWrapper > .list-name {
+                font-weight: 700;
+                font-size: 125%;
+                margin-bottom: 10px;
+            }
+            .mcFormWrapper > .short-code,
+            .mcFormWrapper > .filter-help {
+                background-color: #333;
+                color: #eee;
+                padding: 5px 10px;
+                text-align: right;
+            }
+            .mcFormWrapper > .field {
+                padding: 5px 0;
+                border: 1px solid #aaa;
+                border-top: none;
+            }
+            .mcFormWrapper > .field.titles {
+                background-color: #0074a2;
+                border-top: 1px solid #aaa;
+                color: #fff;
+                font-size: 115%;
+                font-weight: bold;
+            }
+            .mcFormWrapper > .field {
+                background-color: #fff; 
+            }
+            .mcFormWrapper,
+            .mcFormWrapper > .field:after {
+                content: "";
+                display: block;
+                clear: both;
+            }
+            .mcFormWrapper > .field > div {
+                display: inline-block;
+                text-align: center;
+                width: 16.6%;
+                float: left;
+            }
+            .mcFormWrapper > .field > .required {
+                width: 13%;
+            }
+            .mcFormWrapper > .field > .include {
+                width: 13%;
+            }
+            .mcFormWrapper > .field > .placeholder,
+            .mcFormWrapper > .field > .class {
+                width: 20%;
+                margin-right: .4%;
+                min-height: 1px;
+            }
+            .mcFormWrapper > .field > .placeholder > input,
+            .mcFormWrapper > .field > .class > input {
+                width: 100%;
+            }
+            .mcFormWrapper > .field > .success-label,
+            .mcFormWrapper > .field > .failure-label,
+            .mcFormWrapper > .field > .duplicate-failure-label {
+                width: 26%;
+            }
+            .mcFormWrapper .field .success-msg,
+            .mcFormWrapper .field .failure-msg,
+            .mcFormWrapper .field .duplicate-failure-msg {
+                width: 53%;
+            }
+            .mcFormWrapper .field .success-msg input,
+            .mcFormWrapper .field .failure-msg input,
+            .mcFormWrapper .field .duplicate-failure-msg input,
+            .mcFormWrapper .field .msg-class > input {
+                width: 100%;
+            }
+            .mcFormWrapper > .field .msg-class {
+                margin-left: 0.5%;
+                width: 20%;
+            }
+            .mcFormWrapper .mc-form-remove {
+                float: right;
+                background: #A80000;
+                margin: 10px 0 20px 0;
+                padding: 5px 10px;
+                border-radius: 3px;
+                color: white;
+                cursor: pointer;
+            }
+            .mcFormWrapper .mc-form-remove:hover {
+                background: #850000;
+            }
+            .submit {
+                clear: both;
+            }
+        </style>    
+        <?php
+    }
+}
---
+++ includes/js.php
@@ -0,0 +1,112 @@
+<?php
+// Prevent direct access to file
+defined('ABSPATH') or die("No script kiddies please!");
+
+// Injects the OSD Mail Chimp JavaScript into the footer of non-admin pages
+function osd_mail_chimp_js() {
+    ?>
+    <script async='true'>
+        (function() {
+            var osd_mc_forms = document.querySelectorAll('.osd-mc-form-wrapper');
+            var xhrs = [];
+            for (var i=0, l=osd_mc_forms.length; i < l; i++) {
+                var id = osd_mc_forms[i].getAttribute('data-form-id');
+                var submit_text = osd_mc_forms[i].getAttribute('data-submit-text');
+                var classes = osd_mc_forms[i].getAttribute('data-class');
+
+                var xhr = new XMLHttpRequest();
+                xhrs.push(xhr);
+                var data = "form_id=" + id + "&submit_text=" + submit_text + "&class=" + classes + "&osd_mc_ajax=true&wp_nonce=<?php echo wp_create_nonce('osd_mc_load_form'); ?>" + "&action=osd_mc_load_form";
+                var cont = osd_mc_forms[i];
+                xhr.open("POST", "<?php echo site_url(); ?>/wp-admin/admin-ajax.php");
+                xhr.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+                xhr.onreadystatechange = loadFormHtml;
+                xhr.send(data);
+            }
+
+            // Loads the AJAX response into the form container
+            function loadFormHtml() {
+                if (this.readyState === 4 && this.status === 200) {
+                    var cont = osd_mc_forms[xhrs.indexOf(this)];
+                    cont.innerHTML = this.responseText;
+                    var form = cont.querySelector('.osd-mc-form');
+                    if (form !== null) {
+                        form.onsubmit = submit_mc_form;
+                    }
+                }
+            }
+
+
+            // OSD Mailchimp form submission
+            function submit_mc_form(ev) {
+                ev.preventDefault();
+                var form = this;
+
+                // Client side form validation                    
+                if (HTMLFormElement.prototype.checkValidity !== undefined) {
+                    var fields = form[0].querySelectorAll('input, select');
+                    var valid = true;
+                    for (var i=0, l=fields.length; i < l; i++) {
+                        if (!fields[i].checkValidity()) {
+                            valid = false;
+                            fields[i].parentElement.className += " osd-mc-error";
+                        } else {
+                            fields[i].parentElement.className = fields[i].parentElement.className.replace(" osd-mc-error", "");
+                        }
+                    }
+                    if (valid === false) {
+                        form[0].querySelector('.osd-mc-message').innerHTML = "Sorry, there was an error. Please fill out the form with the correct information.";
+                        return;
+                    }
+                }
+                
+                var messages = form.querySelectorAll('.osd-mc-message');
+                for (var i=0, l=messages.length; i < l; i++) {
+                    messages[i].innerHTML = "Processing...";                    
+                }
+                var data = jQuery(form).serialize();
+                data += "&osd_mc_ajax=true&wp_nonce=<?php echo wp_create_nonce('osd_mc_subscribe'); ?>&action=osd_mc_subscribe"
+                var message = "Sorry, there was an error processing your request.";
+
+                var xhr = new XMLHttpRequest();
+                xhr.open("POST", "<?php echo site_url(); ?>/wp-admin/admin-ajax.php");
+                xhr.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+                xhr.onreadystatechange = function() {
+                    if (this.readyState === 4) {
+                        if (this.status === 200) {
+                            var response = (this.response !== undefined) ? this.response : this.responseText;
+                            try {
+                                var returnArray = JSON.parse(response);
+
+                                if (returnArray.redirect !== undefined) {
+                                    window.location = returnArray.url;
+                                    return;
+                                }
+
+                                for (var i=0, l=messages.length; i < l; i++) {
+                                    messages[i].className = "osd-mc-message " + returnArray.class;
+                                    messages[i].innerHTML = returnArray.content;
+                                }
+
+                                if (returnArray.status == 'success') {
+                                    form.reset();    
+                                };
+                            } catch(error) {
+                                for (var i=0, l=messages.length; i < l; i++) {
+                                    messages[i].innerHTML = message;
+                                }
+                            }
+                        } else {
+                            for (var i=0, l=messages.length; i < l; i++) {
+                                messages[i].innerHTML = message;
+                            }
+                        }
+                    }
+                }
+                xhr.send(data);
+            }
+        })();
+    </script>
+    <?php
+}
+add_action('wp_footer', 'osd_mail_chimp_js');
